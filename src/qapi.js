3e580d8c65e7cc168e6459a81c9385b841ff6a07
Rajan Alagarsamy <rajan.alagarsamy@gmail.com>
2023-02-24 17:23:08 +0100

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63f8e3e6b4ef2400206fcb16",
+				"63f8e3e6b4ef2400206fcb19"
+			]
+		},
+		"63f8e3e6b4ef2400206fcb16": {
+			"id": "63f8e3e6b4ef2400206fcb16",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63f8e3e6b4ef2400206fcb19": {
+			"id": "63f8e3e6b4ef2400206fcb19",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
